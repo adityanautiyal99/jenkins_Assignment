e5c2a914947604af861d9ab2ecaffc6d61df4f18
Aditya Nautiyal <77630833+adityanautiyal99@users.noreply.github.com>
2021-05-07 13:04:02 +0530

    Create jenkins.groovy

---
+++ jenkins.groovy
@@ -0,0 +1,98 @@
+pipeline {
+    agent any
+    stages {
+        stage('cloning spring3hibernate and compiling the code') {
+            steps {
+                git 'https://github.com/opstree/spring3hibernate.git'
+                sh "mvn clean"
+                sh "mvn compile"
+            }
+        }
+        stage('Testing the code and creating package') {
+            steps {
+                sh "mvn test"
+                sh "mvn package"
+            }
+        }
+        stage('sending test analysis to SonarQube') {
+            steps {
+                withSonarQubeEnv('sonar') {
+                    sh "mvn sonar:sonar"
+                }
+            }
+        }
+        stage('ArchiveArtifact'){
+            steps {
+                    sh "mvn surefire-report:report"
+                    archiveArtifacts 'target/*.war'   
+            }
+        }
+        /*stage("Quality Gate") {
+            steps {
+              timeout(time: 1, unit: 'HOURS') {
+                waitForQualityGate abortPipeline: false
+              }
+            }
+        }*/
+       stage("Uploading Artifact"){
+            steps{
+                rtServer (
+                id: 'key',
+                url: 'http://0b6619bb95b0.ngrok.io/artifactory',
+                // If you're using username and password:
+                username: 'admin',
+                password: '6DE@Dpool9',
+                // If you're using Credentials ID:
+                //credentialsId: 'ccrreeddeennttiiaall',
+                // Configure the connection timeout (in seconds).
+                // The default value (if not configured) is 300 seconds:
+                timeout: 300
+                )
+                
+                rtUpload (
+                serverId: 'key',
+                spec: '''{
+                      "files": [
+                        {
+                          "pattern": "target/*.war",
+                          "target": "key/Build_${BUILD_NUMBER}/"
+                        }
+                     ]
+                }'''
+                )
+                rtDownload (
+                serverId: 'key',
+                spec: '''{
+                        "files": [
+                        {
+                            "pattern": "key/Build_${BUILD_NUMBER}/",
+                            "target": "aditya/Build_${BUILD_NUMBER}"
+                        }
+                    ]
+                }'''
+                )
+            }
+        }
+    }
+    post {
+        success {
+            notifySuccessful()
+        }
+        failure{
+            notifyFailed()
+        }
+        always{
+            publishHTML(target : [allowMissing: false, alwaysLinkToLastBuild: true, includes: '**/*.html', keepAll: false, reportDir: '/var/lib/jenkins/workspace/pi/target/site', reportFiles: 'index.html', reportName: 'HTML Report', reportTitles: ''])
+        }
+    }
+}
+def notifyFailed() {
+    slackSend (color: '#00FF00', message: "Failed: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
+}
+def notifySuccessful() {
+    slackSend (color: '#00FF00', message: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
+    build job: 'upload',
+        parameters: [
+            string(name: 'BUILD_NU', value: 'Build_' + String.valueOf(env.BUILD_NUMBER))
+        ]
+}
